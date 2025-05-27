13cf77d383d99bc9f433b0a6fcd770437a2b6bed
PXone20202 <lennart.assmann@icloud.com>
2025-05-27 11:52:59 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,48 +1,51 @@
-function Start () {
-    rechts = 0
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
     basic.showLeds(`
         . . . . .
-        . . # . .
-        . . # . .
-        . . # . .
+        . # # # .
+        . # . # .
+        . # # # .
         . . . . .
         `)
-    while (input.buttonIsPressed(Button.B)) {
-        zahl += 1
-    }
-    while (input.buttonIsPressed(Button.A)) {
-        zahl += -1
-    }
-}
-let zahl = 0
-let rechts = 0
-basic.showString("Hallo")
-basic.showLeds(`
-    # # # # #
-    # . . . #
-    # . . . #
-    # . . . #
-    # # # # #
-    `)
-basic.showLeds(`
-    . . . . .
-    . # # # .
-    . # . # .
-    . # # # .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-Start()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
